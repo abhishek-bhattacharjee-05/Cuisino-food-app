0ad4361da495b06354c061e1f6999d00f1c0b21f
abhishekcodes05 <abhishekbhattacharjee200@gmail.com>
2024-08-30 15:46:43 +0530

    new commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://cuisino-food-backend.vercel.app/"
+    const url = "https://cuisino-food-app.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
